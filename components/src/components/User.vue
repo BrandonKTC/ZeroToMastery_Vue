eaea9482e438c514f8fb3044dcb5f9c88816e0fa
BrandonKTC <brandonkwamou@gmail.com>
2022-08-02 10:05:57 +0200

    vue components

---
+++ components/src/components/User.vue
@@ -0,0 +1,38 @@
+<template>
+<button @click="onClickAge">Update Age</button>
+<button @click="ageChangeFn(3)">Update Age CB</button>
+ <p>The User is {{ age }} years old</p>
+ <p>{{ ageDoubled }}</p>
+</template>
+
+<script>
+export default {
+ name: "User",
+ props: {
+  age: {
+   type: Number,
+   // required: true,
+   // default: 20
+   validator(value) {
+    return value < 130
+   }
+  },
+  ageChangeFn: Function
+ },
+ emits: ['age-change'],
+ computed: {
+  ageDoubled() {
+   return this.age * 2
+  }
+ },
+ methods: {
+  onClickAge() {
+   this.$emit('age-change', 3)
+  }
+ }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
