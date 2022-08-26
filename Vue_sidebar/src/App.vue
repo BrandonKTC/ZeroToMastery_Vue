b55137df6c26d0563befe5e631846d9354477273
BrandonKTC <brandonkwamou@gmail.com>
2022-08-26 14:49:08 +0200

    Vue real slots

---
+++ Vue_sidebar/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="app">
+		<hello-world #default="{user, favorites}">
+			<p>Hello {{user.name}}</p>
+			<ul v-for="item in favorites">
+				<li>
+					{{ item }}
+				</li>
+			</ul>
+		</hello-world>
+	</div>
+</template>
+
+<script>
+	import HelloWorld from './components/HelloWorld.vue'
+
+	export default {
+		name: 'App',
+		components: {
+			HelloWorld,
+		},
+	}
+</script>
